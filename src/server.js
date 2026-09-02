7d569a1adc19978e7714f3ad8219506d817042f4
Abirame Ramachandran <abikrithika@gmail.com>
2026-09-02 18:17:49 +0200

    feat: connect application to PostgreSQL

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
+require("dotenv").config();
+
 const express=require("express");
+
+const pool=require("./config/db");
 const app=express();
 const PORT=3000;
 
@@ -6,7 +10,17 @@ app.get('/',(req,res)=>{
     res.send(`Event Ticket Booking API is running`);
 });
 
+pool
+.query("SELECT NOW()")
+.then((result)=>{
+    console.log("Database connected:",result.rows[0].now);
+
+
 app.listen(PORT,()=>{
     console.log(`Server running on this port ${PORT}`);
 });
 
+})
+.catch((error)=>{
+    console.error("Database Connection Failed:",error.message);
+})
