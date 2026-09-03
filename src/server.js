cdcf92c48f82ee3540b0d10fcfd4cf33d9b5c7cd
Abirame Ramachandran <abikrithika@gmail.com>
2026-09-03 11:07:57 +0200

    feat: add get all events endpoint

--- src/server.js
+++ src/server.js
@@ -10,6 +10,19 @@ app.get('/',(req,res)=>{
     res.send(`Event Ticket Booking API is running`);
 });
 
+app.get("/api/events", async(req,res)=>{
+   try{
+   
+        const result=await pool.query("SELECT * FROM event");
+        res.status(200).json(result.rows);
+   }
+   catch (error){
+        console.error("Failed to fetch events:",error.message);
+        res.status(500).json({
+            message:"Internal server error",
+        });
+    }
+});
 pool
 .query("SELECT NOW()")
 .then((result)=>{
